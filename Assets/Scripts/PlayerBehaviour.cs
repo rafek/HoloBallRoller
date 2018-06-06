e283f0ecc10448e548454fb63846d6b5f4c75255
rafek <rafal.legiedz@gmail.com>
2018-06-06 11:40:34 +0200

    Added logic for Player movement and Target behaviour.

---
+++ Assets/Scripts/PlayerBehaviour.cs
@@ -0,0 +1,25 @@
+﻿using HoloToolkit.Unity.InputModule;
+using UnityEngine;
+
+public class PlayerBehaviour : MonoBehaviour, IInputClickHandler
+{
+    public float ForceMultiplier = 10.0f;
+
+    public void OnInputClicked(InputClickedEventData eventData)
+    {
+        Vector3 focusPoint;
+
+        if (Utils.IsFloorPointed(out focusPoint))
+        {
+            var focusPointNorm = new Vector3(focusPoint.x, transform.position.y, focusPoint.z);
+            var playerPoint = transform.position;
+
+            gameObject.GetComponent<Rigidbody>().AddForce((focusPointNorm - playerPoint) * ForceMultiplier);
+        }
+    }
+
+    private void Start()
+    {
+        InputManager.Instance.AddGlobalListener(gameObject);
+    }
+}
